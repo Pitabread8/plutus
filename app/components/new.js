3acc3ee27b77ea0c20d260aed06ad2229b3e725b
Sita Vemuri <sitavemuri8@gmail.com>
2025-10-31 11:29:59 -0700

    improve error logging

--- app/components/new.js
+++ app/components/new.js
@@ -1,6 +1,8 @@
 "use client"
 
-// make date full width on mobile and look nicer; set date to today
+// make date full width on mobile and look nicer; set date to today. 
+// add a way to track reimbursements -- maybe a way to add a list of all ppl who need to pay back, with buttons to mark their status?
+// use onBlur for real-time error validation??
 
 import { useState, useEffect } from "react";
 import { initializeApp } from "firebase/app";
@@ -28,6 +30,7 @@ const ExpenseEntry = ({ user, setOpenNew, openEdit, setOpenEdit }) => {
     const [paymentType, setPaymentType] = useState("");
     const [notes, setNotes] = useState("");
     const [total, setTotal] = useState(0);
+    const [errors, setErrors] = useState({});
 
     const [uploadData, setUploadData] = useState("");
 
@@ -48,7 +51,7 @@ const ExpenseEntry = ({ user, setOpenNew, openEdit, setOpenEdit }) => {
         if (selectedType === "percentage") {
             setTotal(actualAmount + ((actualAmount * actualTip) / 100));
         } else {
-            setTotal(actualAmount + actualTip);
+            setTotal(parseFloat(actualAmount + actualTip).toFixed(2));
         }
     });
 
@@ -83,11 +86,42 @@ const ExpenseEntry = ({ user, setOpenNew, openEdit, setOpenEdit }) => {
         return inputDate <= today;
     };
 
+    const validateData = () => {
+        const newErrors = {};
+
+        if (date === ""|| !isDateAfterToday(date)) {
+            newErrors.date = true;
+        }
+
+        if (paymentType === "") {
+            newErrors.paymentType = true;
+        }
+
+        if (purchase === "") {
+            newErrors.purchase = true;
+        }
+
+        if (amount <= 0 || Number.isNaN(amount)) {
+            newErrors.amount = true;
+        }
+
+        if (tip < 0 || Number.isNaN(tip)) {
+            newErrors.tip = true;
+        }
+
+        setErrors(newErrors);
+
+        if (Object.keys(newErrors).length > 0) {
+            return false;
+        }
+
+        return true;
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        if (date === "" || !isDateAfterToday(date) || purchase === "" || amount <= 0 || tip < 0 || paymentType === "") {
-            alert("Please fill in required fields with valid information.");
+        if (validateData()) {
             return;
         }
 
@@ -138,7 +172,7 @@ const ExpenseEntry = ({ user, setOpenNew, openEdit, setOpenEdit }) => {
     return (
         <div className="w-full h-full backdrop-blur-md fixed top-0 left-0">
             <div className="w-3/4 md:w-1/2 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] bg-neutral-200 dark:bg-neutral-900 p-4 md:p-8 rounded-lg shadow-md">
-                <h2 className="text-base md:text-2xl font-bold mb-6 dark:text-neutral-100">Expense Entry<span className="hidden md:inline"> - ${parseFloat(total).toFixed(2)}</span></h2>
+                <h2 className="text-base md:text-2xl font-bold mb-6 dark:text-neutral-100">Expense Entry<span className="hidden md:inline"> - ${total}</span></h2>
                 <button className="bg-red-500 hover:bg-red-700 text-neutral-100 text-xs md:text-sm font-bold py-1 px-2 md:py-2 md:px-4 m-4 md:m-7 fixed top-0 right-0 rounded" onClick={cancel}>
                     Cancel
                 </button>
@@ -152,7 +186,7 @@ const ExpenseEntry = ({ user, setOpenNew, openEdit, setOpenEdit }) => {
                             onChange={(e) => setDate(e.target.value)}
                             id="date_selector"
                             name="date_selector"
-                            className="mt-1 block w-full p-2 border border-neutral-900 dark:border-neutral-100 bg-neutral-200 dark:bg-neutral-900 rounded-md text-xs md:text-sm h-[3em]"
+                            className={`mt-1 block w-full p-2 border bg-neutral-200 dark:bg-neutral-900 rounded-md text-xs md:text-sm h-[3em] ${errors.date ? "border-red-500" : "border-neutral-900 dark:border-neutral-100"}`}
                         />
                     </div>
                     <div>
@@ -162,25 +196,25 @@ const ExpenseEntry = ({ user, setOpenNew, openEdit, setOpenEdit }) => {
                             onChange={(e) => setPurchase(e.target.value)}
                             id="purchase_selector"
                             name="purchase_selector"
-                            className="mt-1 block w-full h-[3em] p-2 border border-neutral-900 dark:border-neutral-100 bg-neutral-200 dark:bg-neutral-900 rounded-md text-xs md:text-sm"
+                            className={`mt-1 block w-full p-2 border bg-neutral-200 dark:bg-neutral-900 rounded-md text-xs md:text-sm h-[3em] ${errors.purchase ? "border-red-500" : "border-neutral-900 dark:border-neutral-100"}`}
                         />
                     </div>
                     <div className="flex flex-row md:flex-col gap-4">
                         <div className="w-1/2 md:w-full">
                             <label className="block text-xs md:text-sm font-medium dark:text-neutral-400" htmlFor="payment_selector">Payment Type*</label>
                             <div className="mt-1 w-full hidden md:flex flex-row items-center gap-2 text-xs md:text-base">
-                                <button type="button" className={`border border-neutral-900 dark:border-neutral-100 hover:opacity-100 hover:bg-neutral-700 font-medium py-1 px-4 rounded-xl ${paymentType === "Cash" ? "opacity-100 bg-neutral-700" : "opacity-50"}`} onClick={() => setPaymentType("Cash")}>Cash</button>
-                                <button type="button" className={`border border-neutral-900 dark:border-neutral-100 hover:opacity-100 hover:bg-neutral-700 font-medium py-1 px-4 rounded-xl ${paymentType === "Credit" ? "opacity-100 bg-neutral-700" : "opacity-50"}`} onClick={() => setPaymentType("Credit")}>Credit</button>
-                                <button type="button" className={`border border-neutral-900 dark:border-neutral-100 hover:opacity-100 hover:bg-neutral-700 font-medium py-1 px-4 rounded-xl ${paymentType === "Debit" ? "opacity-100 bg-neutral-700" : "opacity-50"}`} onClick={() => setPaymentType("Debit")}>Debit</button>
-                                <button type="button" className={`border border-neutral-900 dark:border-neutral-100 hover:opacity-100 hover:bg-neutral-700 font-medium py-1 px-4 rounded-xl ${paymentType === "Apple Cash" ? "opacity-100 bg-neutral-700" : "opacity-50"}`} onClick={() => setPaymentType("Apple Cash")}>Apple Cash</button>
-                                <button type="button" className={`border border-neutral-900 dark:border-neutral-100 hover:opacity-100 hover:bg-neutral-700 font-medium py-1 px-4 rounded-xl ${paymentType === "Other" ? "opacity-100 bg-neutral-700" : "opacity-50"}`} onClick={() => setPaymentType("Other")}>Other</button>
+                                <button type="button" className={`border hover:opacity-100 hover:bg-neutral-700 font-medium py-1 px-4 rounded-xl ${paymentType === "Cash" ? "opacity-100 bg-neutral-700" : "opacity-50"} ${errors.paymentType ? "border-red-500" : "border-neutral-900 dark:border-neutral-100"}`} onClick={() => setPaymentType("Cash")}>Cash</button>
+                                <button type="button" className={`border hover:opacity-100 hover:bg-neutral-700 font-medium py-1 px-4 rounded-xl ${paymentType === "Credit" ? "opacity-100 bg-neutral-700" : "opacity-50"} ${errors.paymentType ? "border-red-500" : "border-neutral-900 dark:border-neutral-100"}`} onClick={() => setPaymentType("Credit")}>Credit</button>
+                                <button type="button" className={`border hover:opacity-100 hover:bg-neutral-700 font-medium py-1 px-4 rounded-xl ${paymentType === "Debit" ? "opacity-100 bg-neutral-700" : "opacity-50"} ${errors.paymentType ? "border-red-500" : "border-neutral-900 dark:border-neutral-100"}`} onClick={() => setPaymentType("Debit")}>Debit</button>
+                                <button type="button" className={`border hover:opacity-100 hover:bg-neutral-700 font-medium py-1 px-4 rounded-xl ${paymentType === "Apple Cash" ? "opacity-100 bg-neutral-700" : "opacity-50"} ${errors.paymentType ? "border-red-500" : "border-neutral-900 dark:border-neutral-100"}`} onClick={() => setPaymentType("Apple Cash")}>Apple Cash</button>
+                                <button type="button" className={`border hover:opacity-100 hover:bg-neutral-700 font-medium py-1 px-4 rounded-xl ${paymentType === "Other" ? "opacity-100 bg-neutral-700" : "opacity-50"} ${errors.paymentType ? "border-red-500" : "border-neutral-900 dark:border-neutral-100"}`} onClick={() => setPaymentType("Other")}>Other</button>
                             </div>
                             <select
                                 value={paymentType}
                                 onChange={(e) => setPaymentType(e.target.value)}
                                 id="payment_selector"
                                 name="payment_selector"
-                                className="mt-1 block md:hidden w-full p-2 border border-neutral-900 dark:border-neutral-100 bg-neutral-200 dark:bg-neutral-900 rounded-md text-xs md:text-sm h-[3em]"
+                                className={`mt-1 block w-full p-2 border bg-neutral-200 dark:bg-neutral-900 rounded-md text-xs md:text-sm h-[3em] md:hidden ${errors.paymentType ? "border-red-500" : "border-neutral-900 dark:border-neutral-100"}`}
                             >
                                 <option value="" disabled>Select</option>
                                 <option value="Cash">Cash</option>
@@ -200,7 +234,7 @@ const ExpenseEntry = ({ user, setOpenNew, openEdit, setOpenEdit }) => {
                                     onChange={(e) => setAmount(parseFloat(e.target.value))}
                                     id="amount_selector"
                                     name="amount_selector"
-                                    className="mt-1 block w-full p-2 border border-neutral-900 dark:border-neutral-100 bg-neutral-200 dark:bg-neutral-900 rounded-md text-xs md:text-sm h-[3em]"
+                                    className={`mt-1 block w-full p-2 border bg-neutral-200 dark:bg-neutral-900 rounded-md text-xs md:text-sm h-[3em] ${errors.amount ? "border-red-500" : "border-neutral-900 dark:border-neutral-100"}`}
                                 />
                             </div>
                         </div>
@@ -212,10 +246,10 @@ const ExpenseEntry = ({ user, setOpenNew, openEdit, setOpenEdit }) => {
                             <input
                                 type="number"
                                 value={tip}
-                                onChange={(e) => setTip(parseFloat(e.target.value))}
+                                onChange={(e) => setTip(parseFloat(e.target.value) != NaN ? parseFloat(e.target.value) : 0)}
                                 id="tip_selector"
                                 name="tip_selector"
-                                className="mt-1 block w-full p-2 border border-neutral-900 dark:border-neutral-100 bg-neutral-200 dark:bg-neutral-900 rounded-md text-xs md:text-sm h-[3em]"
+                                className={`mt-1 block w-full p-2 border bg-neutral-200 dark:bg-neutral-900 rounded-md text-xs md:text-sm h-[3em] ${errors.tip ? "border-red-500" : "border-neutral-900 dark:border-neutral-100"}`}
                             />
                             <button type="button" className={`text-xl ${selectedType === "percentage" ? "opacity-100" : "opacity-50"}`} onClick={() => setSelectedType("percentage")}>%</button>
                         </div>
